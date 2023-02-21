3c9dc5dee5dbd712b62fcec0729bb7b1d20fc9c5
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2023-02-21 05:23:12 -0600

    [Add]: application with navigation and connect products

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,14 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+//import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
-import PRODUCTS from "../shop-data.json";
+//import SHOP_DATA from "../shop-data.js";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
   const value = { products };
+
+  /*useEffect(() => {
+    addCollectionAndDocuments("categories", SHOP_DATA);
+  }, []);*/
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
+
   return (
     <ProductsContext.Provider value={value}>
       {children}
