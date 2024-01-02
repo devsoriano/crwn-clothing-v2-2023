b26860f2ee29e83bf7a9808b40ef556a305779e2
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2024-01-02 17:58:40 -0600

    [Add]: Redux extended tools

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./store/store";
+import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
